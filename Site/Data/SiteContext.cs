161773ccf9b0a77fe61b492ba6d0223c3dd9905e
Fernando Almeida <contatofernandoalmeida@hotmail.com>
2021-01-14 19:28:08 -0300

    erro ajustado

---
+++ Site/Data/SiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Site.Models;
+
+namespace Site.Data
+{
+    public class SiteContext : DbContext
+    {
+        public SiteContext (DbContextOptions<SiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Site.Models.Department> Department { get; set; }
+    }
+}
--- Site/Startup.cs
+++ Site/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Site.Data;
 
 namespace Site
 {
@@ -33,6 +35,9 @@ namespace Site
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SiteContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SiteContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
