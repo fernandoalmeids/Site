c7e11429be64e170c4eb23a8210a9014bf3365c0
Fernando Almeida <contatofernandoalmeida@hotmail.com>
2021-01-15 20:10:20 -0300

    alterado boot e criado classes

--- Site/Models/Department.cs
+++ Site/Models/Department.cs
@@ -1,8 +1,22 @@
-﻿namespace Site.Models
+﻿using System.Collections.Generic;
+
+namespace Site.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(); //Um departamento tem varios vendedores
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
---
+++ Site/Models/Enums/SaleStatus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Site.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Billed = 1, //faturado
+        Canceled = 2
+    }
+}
